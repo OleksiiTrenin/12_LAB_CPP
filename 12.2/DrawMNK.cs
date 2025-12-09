729ab9b69cf88d9f22a0ffdf8946538b9c9c34f1
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-12-09 11:27:40 +0200

    Add files via upload

---
+++ 12.2/DrawMNK.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class DrawMNK
+    {
+        double kx, ky, zx, zy;
+        int Tx(double x) => (int)(kx * x + zx);
+        int Ty(double y) => (int)(ky * y + zy);
+
+        public void BuildMNK(PictureBox pb, double[] xe, double[] ye, double[] xs, double[] ys, int ne, int ngr)
+        {
+            Graphics g = pb.CreateGraphics();
+            g.Clear(Color.White);
+
+            double minx = xs[0], maxx = xs[0];
+            double miny = ys[0], maxy = ys[0];
+
+            for (int i = 1; i < ngr; i++)
+            {
+                if (xs[i] < minx) minx = xs[i];
+                if (xs[i] > maxx) maxx = xs[i];
+                if (ys[i] < miny) miny = ys[i];
+                if (ys[i] > maxy) maxy = ys[i];
+            }
+
+            for (int i = 0; i < ne; i++)
+            {
+                if (xe[i] < minx) minx = xe[i];
+                if (xe[i] > maxx) maxx = xe[i];
+                if (ye[i] < miny) miny = ye[i];
+                if (ye[i] > maxy) maxy = ye[i];
+            }
+
+            kx = (pb.Width - 20) / (maxx - minx);
+            ky = (pb.Height - 20) / (miny - maxy);
+            zx = (pb.Width * minx - 10 * (minx + maxx)) / (minx - maxx);
+            zy = (pb.Height * maxy - 10 * (miny + maxy)) / (maxy - miny);
+
+
+
+
+
+            Pen grid = new Pen(Color.LightGray);
+            grid.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+
+            for (int i = 0; i < pb.Width; i += 40)
+                g.DrawLine(grid, i, 0, i, pb.Height);
+
+            for (int i = 0; i < pb.Height; i += 40)
+                g.DrawLine(grid, 0, i, pb.Width, i);
+
+            Pen ax = new Pen(Color.Black, 2);
+            g.DrawLine(ax, Tx(0), 0, Tx(0), pb.Height);
+            g.DrawLine(ax, 0, Ty(0), pb.Width, Ty(0));
+
+            Pen pExp = new Pen(Color.Red, 2);
+            for (int i = 0; i < ne; i++)
+                g.DrawRectangle(pExp, Tx(xe[i]) - 3, Ty(ye[i]) - 3, 6, 6);
+
+            Pen pApr = new Pen(Color.Red, 2);
+            for (int i = 1; i < ngr; i++)
+                g.DrawLine(pApr, Tx(xs[i - 1]), Ty(ys[i - 1]), Tx(xs[i]), Ty(ys[i]));
+        }
+    }
+}
---
+++ 12.2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DrawMNK dm = new DrawMNK();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFill_Click(object sender, EventArgs e)
+        {
+            int ne = int.Parse(textNe.Text);
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < ne; i++)
+                dataGridView1.Rows.Add();
+        }
+
+        private void buttonApprox_Click(object sender, EventArgs e)
+        {
+            int ne = int.Parse(textNe.Text);
+            int ngr = int.Parse(textNgr.Text);
+            int m = int.Parse(textM.Text);
+
+           
+            for (int i = 0; i < ne; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value == null ||
+                    dataGridView1.Rows[i].Cells[1].Value == null ||
+                    dataGridView1.Rows[i].Cells[0].Value.ToString() == "" ||
+                    dataGridView1.Rows[i].Cells[1].Value.ToString() == "")
+                {
+                    MessageBox.Show("Фіча! Не заповнено!",
+                        "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            double[] xe = new double[ne];
+            double[] ye = new double[ne];
+
+            for (int i = 0; i < ne; i++)
+            {
+                xe[i] = double.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString().Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+
+                ye[i] = double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString().Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+            }
+
+            double[] k = new double[m + 1];
+            Method_Aprox(xe, ye, ne, ref k, m);
+
+            double minx = xe[0], maxx = xe[0];
+            for (int i = 1; i < ne; i++)
+            {
+                if (xe[i] < minx) minx = xe[i];
+                if (xe[i] > maxx) maxx = xe[i];
+            }
+
+            double[] xs = new double[ngr];
+            double[] ys = new double[ngr];
+
+            double h = (maxx - minx) / (ngr - 1);
+            xs[0] = minx;
+
+            for (int i = 0; i < ngr; i++)
+            {
+                ys[i] = 0;
+                for (int j = 0; j <= m; j++)
+                    ys[i] += k[j] * Math.Pow(xs[i], j);
+
+                if (i < ngr - 1) xs[i + 1] = xs[i] + h;
+            }
+
+            dm.BuildMNK(pictureBox1, xe, ye, xs, ys, ne, ngr);
+        }
+
+        bool Method_Aprox(double[] x, double[] y, int n, ref double[] k, int m)
+        {
+            double[,] A = new double[m + 1, m + 2];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= m; j++)
+                {
+                    double s = 0;
+                    for (int t = 0; t < n; t++)
+                        s += Math.Pow(x[t], i + j);
+                    A[i, j] = s;
+                }
+
+                double s2 = 0;
+                for (int t = 0; t < n; t++)
+                    s2 += y[t] * Math.Pow(x[t], i);
+                A[i, m + 1] = s2;
+            }
+
+            for (int i = 0; i <= m; i++)
+            {
+                double v = A[i, i];
+                for (int j = i; j <= m + 1; j++)
+                    A[i, j] /= v;
+
+                for (int r = 0; r <= m; r++)
+                {
+                    if (r == i) continue;
+                    double v2 = A[r, i];
+                    for (int c = i; c <= m + 1; c++)
+                        A[r, c] -= v2 * A[i, c];
+                }
+            }
+
+            for (int i = 0; i <= m; i++)
+                k[i] = A[i, m + 1];
+
+            return true;
+        }
+    }
+}
